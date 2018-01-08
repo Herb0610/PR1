8ab5887001993db2a37eb0a0c94e3e46545f91a4
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2018-01-08 19:44:42 +0100

    beispiel rezept

---
+++ 20180108-Bsp-Rezept/src/rezept/DemoApp.java
@@ -0,0 +1,22 @@
+package rezept;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+
+		Zutat[] zutaten = new Zutat[4];
+		zutaten[0] = new Zutat("Mehl", 300);
+		zutaten[1] = new Zutat("Butter", 300);
+		zutaten[2] = new Zutat("Zucker", 300);
+		zutaten[3] = new Zutat("Eier", 2);
+		
+		Rezept keks = new Rezept("Keks", (byte) 4, zutaten);
+		
+		keks.printRezept();
+		
+		Rezept keks8 = keks.umrechnen((byte)8);
+		keks8.printRezept();
+		
+	}
+
+}
