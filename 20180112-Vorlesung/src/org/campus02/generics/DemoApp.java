baae36f357e1dccdcc0e7e2c775c05cdac5d969d
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2018-01-12 20:17:06 +0100

    implementation of stack

--- 20180112-Vorlesung/src/org/campus02/generics/DemoApp.java
+++ 20180112-Vorlesung/src/org/campus02/generics/DemoApp.java
@@ -4,10 +4,31 @@ public class DemoApp {
 
 	public static void main(String[] args) {
 
+		Integer[] arr = new Integer[5];
+		Stack<Integer> stack = new Stack<Integer>(arr);
 		
+		stack.push(5);
+		stack.push(4);
+		stack.push(3);
+		stack.push(2);
+		stack.print();
 		
+		System.out.println("Ergebnis von pop: " + stack.pop());
 		
+		stack.print();
 		
+		System.out.println("Ergebnis von pop: " + stack.pop());
+		
+		stack.print();
+		
+		stack.push(8);
+		stack.print();
+		
+		Stack<String> names = new Stack<String>(new String[5]);
+		names.push("John");
+		names.push("Max");
+		names.push("Susi");
+		names.print();
 	}
 
 }
--- 20180112-Vorlesung/src/org/generic/cage/DemoApp.java
+++ 20180112-Vorlesung/src/org/generic/cage/DemoApp.java
@@ -1,9 +1,18 @@
 package org.generic.cage;
 
+import org.generic.animals.Cat;
+import org.generic.animals.Dog;
+
 public class DemoApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Cat schnurli = new Cat("schnurli");
+		Dog bello = new Dog("bello", 5000);
+		
+		Cage<Dog> cageDog = new Cage<Dog>(bello);
+		Cage<Cat> cageCat = new Cage<Cat>(schnurli);
+		
 
 	}
 
